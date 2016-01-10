57ce1f4933f6dbcc25d3c39f79e6fef2a52d79f6
cgel <carlesgelada@hotmail.com>
2016-01-10 12:40:14 +0100

    input read from joystick

--- ground.cpp
+++ ground.cpp
@@ -4,6 +4,7 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/array.hpp>
 #include <string>
+#include "joystick.hh"
 
 #define BUGGY_IP "127.0.0.1"
 #define PORT 6000
@@ -29,13 +30,44 @@ private:
   udp::endpoint buggy_endpoint;
 };
 
+class Command {
+public:
+  Command() : joystick("/dev/input/js0") {}
+
+  void sample() {
+    int count = 0;
+    while (count < 20) {
+      ++count;
+      usleep(100);
+      JoystickEvent event;
+      if (joystick.sample(&event)) {
+        // if (event.isAxis() && (event.number == 0 || event.number == 13))
+        if (event.number == 0 || event.number == 13) {
+          int val = (int)(event.value * (256.0 / 32767.0));
+          if (event.number == 13)
+            input[0] = val;
+          else
+            input[1] = val;
+        }
+      }
+    }
+  }
+
+  boost::array<short int, 2> input;
+
+private:
+  Joystick joystick;
+};
+
 int main(int argc, char *argv[]) {
   Comms comms;
+  Command cmd;
   try {
     while (1) {
-      boost::array<short int, 2> msg = { 0, 1 };
-      comms.send(msg);
-      sleep(1);
+      cmd.sample();
+      comms.send(cmd.input);
+      usleep(10000);
+      std::cout << cmd.input[0] << " - " << cmd.input[1] << std::endl;
     }
   }
   catch (std::exception &e) {
